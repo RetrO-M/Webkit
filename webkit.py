bc708f4c35d3b7575a08fe0583713bd09e43c77b
Fatal.r00ted <128257558+RetrO-M@users.noreply.github.com>
2024-07-09 19:34:37 +0200

    Update webkit.py

--- webkit.py
+++ webkit.py
@@ -1,3 +1,30 @@
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣤⣤⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣾⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⡿⣷⡀⠀⠀⠀⠀⠀⠀
+⢸⣿⣿⣿⡇⣿⣿⣿⣿⣿⢱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠘⣿⣿⣷⢻⣿⣿⣿⡙⣷⡀⠀⠀⠀⠀⠀
+⣼⣿⣿⣿⡇⣿⣿⣿⣿⡇⣿⣿⣿⣿⡿⣿⣿⡿⣹⣿⣿⣿⠇⠀⠘⣿⣿⡆⣿⣿⣿⣧⠘⢷⠀⠀⠀⠀⠀
+⢹⣿⣿⣿⡇⢿⣿⣿⣿⢰⣿⣿⣿⠟⢲⣿⣟⠁⣿⣿⣿⡟⢀⡠⠔⠘⣿⣧⢸⣿⣿⣿⡄⠈⣇⠀⠀⠀⠀
+⢸⣿⣿⣿⣷⢸⣿⣿⣿⢸⣿⣟⣁⣠⣿⣋⡈⢻⣿⣿⡟⠘⣁⣴⣒⣒⣺⣿⢸⣿⣿⣿⡇⠀⠈⠀⠀⠀⠀
+⠈⣿⣿⣿⣿⡆⢿⡟⣿⢸⡟⣙⣿⣿⣿⡇⢁⣿⣿⠟⠀⠀⢩⣭⣿⣿⡟⣿⢸⣿⣿⢿⡇⠀⠀⠀⠀⠀⠀
+⠀⠸⣿⣿⣿⣿⡜⣿⡸⣾⡇⠻⠿⠿⠟⠀⣼⡿⠋⠀⠀⠀⠀⠙⠛⠟⠃⢸⣿⣿⣿⢸⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⢹⡿⣿⣿⣿⣞⢷⣽⡁⠀⠀⠀⢀⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣸⣿⡏⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠱⠙⢿⡿⣿⣿⣷⣑⠀⠀⠀⠈⠀⠀⣀⣀⣤⠄⠀⠀⠀⠀⠀⣀⣤⢟⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠻⢌⠛⢿⣝⠿⢿⣶⣿⣿⣿⣟⣛⣛⡛⠛⢶⣶⠟⢋⣿⠏⠸⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⡿⠿⠿⣿⠿⣿⣿⣿⣿⢻⠱⣄⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣴⣿⡏⠀⢀⡎⠁⢠⠏⠘⠲⣍⠁⣧⠉⣇⢧⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣼⣿⣿⠃⠀⡼⠀⢠⣎⣀⣀⣀⣹⠀⢸⡀⠸⣌⠆⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠸⠛⠉⠀⠀⢠⣯⢷⣏⣼⣍⣽⣛⣦⣀⣸⠧⣄⣾⣴⠾⠻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡰⣶⣏⣵⣤⠿⣿⣷⣶⣿⠿⡿⠷⣾⣽⣿⣿⠦⠯⢐⡤⢄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⠠⢲⠝⠊⠉⡟⢋⣞⡧⠿⠵⡶⣶⠙⡭⠭⠭⢭⡿⢿⡀⠀⠀⠉⠒⠯⡐⡤⢀⠀⠀⠀⠀
+⠀⠀⡀⠴⢊⠝⠊⠀⠀⠀⢰⠁⠉⢡⡿⠶⣶⣴⠃⠀⢷⣦⡤⣼⡆⠀⠱⡀⠀⠀⠀⠀⠀⠁⠂⢬⡐⠠⡀
+⠀⣰⠅⠊⠀⠀⠀⠀⠀⠀⠈⠦⠔⠁⣸⣓⣿⠏⠀⠀⠸⣿⣷⣾⠘⢄⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠳
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠉⠉⠁⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
+
 from rgbprint import Color, gradient_print
 import requests
 from urllib.parse import urljoin
@@ -7,14 +34,20 @@ import time
 import socket
 import json
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+import socket
+import re
+import io
+
 
 green = Color.pale_green
-red = Color.orange_red
+red = Color.dodger_blue
 magenta = Color.magenta
 white = Color.ghost_white
+red2 = Color.indian_red
 
 success = f"{green}[+]{white}"
-error = f"{red}[-]{white}"
+error = f"{red2}[-]{white}"
 wait = f"{magenta}[*]{white}"
 
 
@@ -160,6 +193,7 @@ def check_paths(base_url, wordlist_file):
             print(f"{error} {url}")
 
 
+
 def check_clickjacking(url):
     try:
         if not url.startswith('http://') and not url.startswith('https://'):
@@ -194,43 +228,131 @@ def port_scan(target_host):
             print(f"{error} Port   |   {port}   |   closed")
 
 
+def get_initial_links(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = soup.find_all('a', href=True)
+        return [(link['href'], 0, datetime.now()) for link in links]
+    except requests.exceptions.RequestException as e:
+        print(f"{error} ERROR REQUESTS : {e}")
+        return []
+
+def monitor_links(url, interval=3600):  
+    links = get_initial_links(url)
+    
+    while True:
+        print(f"{wait} Checking links to {datetime.now()}")
+        
+        for i, (link, last_status, last_check_time) in enumerate(links):
+            try:
+                response = requests.head(link)
+                current_status = response.status_code
+                current_check_time = datetime.now()
+            
+                if current_status != last_status:
+                    print(f"{success} Change detected for           :            {link}         :      {last_status}        ---->      {current_status}")
+
+                links[i] = (link, current_status, current_check_time)
+                
+            except requests.exceptions.RequestException as e:
+                print(f"{error} Error requests for :  {link}: {e}")
+        
+        time.sleep(interval)
+
+def email2(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            pattern = r'[\w\.-]+@[\w\.-]+'
+            emails = set(re.findall(pattern, response.text))
+            return list(emails)
+        else:
+            print(f"{error} Error {response.status_code} Requests")
+    except requests.exceptions.RequestException as e:
+        print(f"{error} ERROR: {e}")
+
+
+def read_sql(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        sql_content = io.BytesIO(response.content)
+        
+        sql_text = sql_content.read().decode('utf-8')
+        
+        return sql_text
+        
+    except requests.exceptions.RequestException as e:
+        print(f"{error} ERROR REQUESTS : {e}")
+        return None
+    except Exception as e:
+        print(f"{error}ERROR : {e}")
+        return None
+
+def dns_lookup(hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except socket.error as e:
+        return f"{error} ERROR DNS : {e}"
 
 
 
-logo = f"""{green}
-  /$$      /$$ /$$$$$$$$ /$$$$$$$  /$$   /$$ /$$$$$$ /$$$$$$$$
- | $$  /$ | $$| $$_____/| $$__  $$| $$  /$$/|_  $$_/|__  $$__/
- | $$ /$$$| $$| $$      | $$  \ $$| $$ /$$/   | $$     | $$   
- | $$/$$ $$ $$| $$$$$   | $$$$$$$ | $$$$$/    | $$     | $$   
- | $$$$_  $$$$| $$__/   | $$__  $$| $$  $$    | $$     | $$   
- | $$$/ \  $$$| $$      | $$  \ $$| $$\  $$   | $$     | $$   
- | $$/   \  $$| $$$$$$$$| $$$$$$$/| $$ \  $$ /$$$$$$   | $$   
- |__/     \__/|________/|_______/ |__/  \__/|______/   |__/  
-
- {white}•{green} Coded by Fatal r00ted
+logo = f"""
+ {white}▒█     ██ ╔▒█▓███ ▒▒═███ {red} ██   ██ █▒ █▒▒▒═██▒
+ {white}▓#     █║ ██      ║█  [██{red} █║  ╔█  █╣    ║█    
+ {white}║▒ ╔█  ██ ▒╩▒▒    █╩███╗ {red} ▒▓██╣"  ▒▒    █▒ 
+ {white}██"▒█╗'██ █║      █║  '▒█{red} ║█  ▓╗  #▓    ▓╣    
+ {white} ▒██ ▓██  █▒#██═█ █▒▒███ {red} ▒▒   █▒ ▒█    ▒█   
 """
 
-help = f"""{white}• {green}sql <URL>             -   {red}SQL Injection Scanner
-{white}• {green}xss <URL>             -   {red}XSS Scanner
-{white}• {green}subdomain <URL>       -   {red}Subdomain Website
-{white}• {green}clickjacking <URL>    -   {red}Clickjacking Scanner
-{white}• {green}get <domain.com>      -   {red}Website Information
-{white}• {green}proxy <ip:port>       -   {red}Proxy HTTP check
-{white}• {green}portscan <domain.com> -   {red}Port Scanner
-{white}• {green}scrape <URL>          -   {red}Web Scraper
-{white}• {green}file <URL>            -   {red}show files
-{white}• {green}read <URL>            -   {red}see all files"""
+help = f"""{white}• {green}sql <URL>              -   {red}SQL Injection Scanner
+{white}• {green}xss <URL>              -   {red}XSS Scanner
+{white}• {green}subdomain <URL>        -   {red}Subdomain Website
+{white}• {green}clickjacking <URL>     -   {red}Clickjacking Scanner
+{white}• {green}get <domain.com>       -   {red}Website Information
+{white}• {green}proxy <ip:port>        -   {red}Proxy HTTP check
+{white}• {green}portscan <domain.com>  -   {red}Port Scanner
+{white}• {green}scrape <URL>           -   {red}Web Scraper
+{white}• {green}file <URL>             -   {red}show files
+{white}• {green}read <URL>             -   {red}see all files
+{white}• {green}admin <URL>            -   {red}Admin Finder
+{white}• {green}flag <URL>             -   {red}Flag Finder
+{white}• {green}spy <URL>              -   {red}monitor a website
+{white}• {green}ip <domain.com>        -   {red}Show site IPs if they have changed
+{white}• {green}email <URL>            -   {red}Recover emails
+{white}• {green}scan <URL>             -   {red}Scan a website to get the flag.txt and root.txt and user.txt and robots.txt
+{white}• {green}rsql <URL/example.sql> -   {red}read a .sql file on a website
+{white}• {green}search <URL/login>     -   {red}search fields
+{white}• {green}dns <domain.com>       -   {red}DNS Lookup
+"""
+
+helpall = f"""{white}> {error}       >>>   If there is that, it's either that the thing doesn't work or doesn't exist or an error
+> {success}       >>>   If there is that, it's just working
+> {wait}       >>>   If there is that, it means there is a loading so you have to wait
+Example :
+{wait} Starting the scanner
+{success} http://google.com/robots.txt
+{success} http://google.com/sms
+{error} http://google.com/flag.txt"""
 
 print(logo)
 
-def main():
 
+def main():
     while True:
         choice = input(f'{white}webkit:~{red}${green} ')
 
         w0rd = choice.split()
         if choice == "help":
             print(help)
+            print(f"{white}• {green}Type : 'help all' ")
+
+        if choice == "help all":
+            print(helpall)
+
         if choice.startswith("sql"):
             if len(w0rd) > 1:
                 word = w0rd[-1]
@@ -250,7 +372,7 @@ def main():
             if len(w0rd) > 1:
                 word = w0rd[-1]
                 base_url = word
-                wordlist_file = 'rockyou.txt'
+                wordlist_file = 'wordlist/rockyou.txt'
                 check_paths(base_url, wordlist_file)
             else:
                 print(f"{error} Try again, you forgot the URL for the Subdomain")
@@ -371,6 +493,131 @@ def main():
                        print(f'{error} Failed to access {base_url} | {e}')
             else:
                 print(f"{error} Try again. Put a URL to read the files")
+
+        if choice.startswith("admin"):
+            if len(w0rd) > 1:
+                word = w0rd[-1] 
+                url2 = word
+                wordlist = 'wordlist/admin.txt'
+                check_paths(url2, wordlist) 
+
+            else:
+                print(f"{error} Try again for Admin Finder")
+
+
+        if choice.startswith("flag"):
+            if len(w0rd) > 1:
+                word = w0rd[-1] 
+                url = word
+                wordlist = 'wordlist/flag.txt'
+                check_paths(url, wordlist) 
+            else:
+                print(f"{error} Try again for Flag Finder")
+
+        if choice.startswith("spy"):
+            if len(w0rd) > 1:
+                word = w0rd[-1] 
+                url = word
+                monitor_links(url)
+            else:
+                print(f"{error} Try again, put a url to monitor")
+
+
+        if choice.startswith("ip"):
+            if len(w0rd) > 1:
+                word = w0rd[-1] 
+                url = word
+                previous_ip = None
+
+                while True:
+                    try:
+                        current_ip = socket.gethostbyname(url)
+                        if previous_ip and previous_ip != current_ip:
+                            print(f"{success} IP Changed for {url}: {current_ip}")
+                        elif not previous_ip:
+                            print(f"{wait} Initial IP for {url}: {current_ip}")
+
+                        previous_ip = current_ip
+                        time.sleep(24 * 60 * 60)
+                    except KeyboardInterrupt:
+                        print("Stop.")
+            else:
+                print(f"{error} Try again to monitor the IP")
+
+        if choice.startswith("email"):
+            if len(w0rd) > 1:
+                word = w0rd[-1] 
+                url = word
+                emails = email2(url)
+                if emails:
+                    print(f"{success} Email addresses found on {url}:")
+                    for email in emails:
+                       print(f'|{green} Found :{red} {email} {white}')
+                else:
+                    print(f"{error} No email address found on : {url}.")
+
+            else:
+                print(f"{error} Try again to receive all emails")
+
+        if choice.startswith("scan"):
+            if len(w0rd) > 1:
+                word = w0rd[-1]
+                url = word
+                url2 = word
+                wordlist = 'wordlist/scan.txt'
+                check_paths(url2, wordlist) 
+            else:
+                print(f"{error} try again to scan the site")
+
+
+
+        if choice.startswith("rsql"):
+            if len(w0rd) > 1:
+                word = w0rd[-1]
+                url = word
+                contenu_sql = read_sql(url)
+
+                if contenu_sql:
+                    print("FILE SQL :")
+                    print(f'{green}{contenu_sql}')
+                else:
+                    print(f"{error} ERROR: {url}")
+            else:
+                print(f"{error} try again, to read a .sql file")
+
+
+        if choice.startswith("search"):
+            if len(w0rd) > 1:
+                word = w0rd[-1]
+                url = word
+                response = requests.get(url)
+
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.content, 'html.parser')
+
+                    input_fields = soup.find_all('input')
+
+                    for field in input_fields:
+                        field_type = field.get('type')
+                        field_name = field.get('name')
+                        field_id = field.get('id')
+                        field_placeholder = field.get('placeholder')
+                        print(f"{success} Type: {field_type}     |     Name: {field_name}     |     ID: {field_id}     |     Placeholder: {field_placeholder}")
+                else:
+                    print(f"{error} Failed to retrieve the page. Status code : {response.status_code}")
+
+            else:
+                print(f"{error} try again to find the fields")
+
+        if choice.startswith("dns"):
+            if len(w0rd) > 1:
+                word = w0rd[-1]
+                hostname = word
+                ip_address = dns_lookup(hostname)
+                print(f"{success} HOSTNAME : {hostname}")
+                print(f"{success} IP : {ip_address}")
+            else:
+                print(f"{error} try again, put a domain for dns lookup")
 
 
 if __name__ == '__main__':
