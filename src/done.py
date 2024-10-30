0c851ff3a7b98fce7bc6094fc3deeee24b11115b
D3m3t0r_ <128257558+RetrO-M@users.noreply.github.com>
2024-10-30 19:45:02 +0100

    Delete src directory

--- src/done.py
+++
@@ -1,9 +0,0 @@
-from sys import stdout
-from rgbprint import Color
-
-white = Color.ghost_white
-blue = Color.sky_blue
-magenta = Color.magenta
-
-def attack_done():
-    stdout.write(f"\n{magenta}[+]{white} Attack Done !\n")
